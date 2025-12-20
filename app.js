d3fd8276cc488d76fef0f6da4825be8d8423a2f8
ZealAI <zeal.ai.app@gmail.com>
2025-12-20 16:49:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-let messages = [];
-messages.push({ role: "user", content: userInput });
-body: JSON.stringify({
-  messages: messages
-})
-messages.push({ role: "assistant", content: data.reply });
-//chatBox.innerHTML = "";
-
-//messages.forEach(msg => {
- // const div = document.createElement("div");
- // div.className = msg.role;
- // div.textContent = msg.content;
-//  chatBox.appendChild(div);
-//});
-
-
 const sendBtn = document.getElementById("send");
 const input = document.getElementById("input");
 const responseBox = document.getElementById("response");
